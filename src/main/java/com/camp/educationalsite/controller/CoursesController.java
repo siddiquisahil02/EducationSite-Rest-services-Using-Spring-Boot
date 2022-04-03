ed8aa13674c24a5ccda91c90ee94fc54dc5fcfd5
Sahil Siddiqui <siddiquisahil02@gmail.com>
2022-04-03 10:25:57 +0530

    Added Spring Security

--- src/main/java/com/camp/educationalsite/controller/CoursesController.java
+++ src/main/java/com/camp/educationalsite/controller/CoursesController.java
@@ -12,6 +12,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,7 @@ public class CoursesController {
     @Autowired
     private ObjectMapper mapper;
     
+    @PreAuthorize("hasAuthority('FACULTY')")
     @GetMapping("/all")
     public List<Courses> getAllCourses(){
         return this.service.getAll();
--- src/main/java/com/camp/educationalsite/controller/StudentController.java
+++ src/main/java/com/camp/educationalsite/controller/StudentController.java
@@ -25,18 +25,21 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 
 @RestController
+@RequestMapping("/students")
 public class StudentController {
 
     @Autowired
@@ -54,6 +57,7 @@ public class StudentController {
     @Autowired
     private UploadAttachments controller;
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("/all")
     public List<ObjectNode> getAllStudents(){
         List<ObjectNode> data = new ArrayList<>();
